8852d28f9190a8b29c3d99816c3d436cd10489a0
Nathan Schrader <natecschrader@gmail.com>
2024-08-05 16:51:36 -0700

    add nuxt configs for css, postcss, alias file names

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
+
 export default defineNuxtConfig({
+  alias: {
+    '@': resolve(__dirname, '/'),
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
