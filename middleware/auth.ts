90f2e917a2a071ddc783b44426f3ba785fe6d6f3
Nathan Schrader <natecschrader@gmail.com>
2024-08-06 14:18:32 -0700

    practice implementing route middleware

---
+++ middleware/auth.ts
@@ -0,0 +1,13 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const isLoggedIn = true;
+  // to is the destination
+  // console.log('TO!!:', to)
+  // if (isLoggedIn) {
+  //   // redirect to the page we want to go
+  //   // redirect to a login page
+  //   return navigateTo('/custom')
+  // }
+
+  // return navigateTo('/custom')
+
+})
